271e335e06735c71e4c5b23fe85139c89a026ff8
Binglin Luo <binglinluo@gmail.com>
2018-12-20 22:28:12 +0800

    add array list map

--- app/src/main/java/com/bingin/databindingtest/MainActivity.java
+++ app/src/main/java/com/bingin/databindingtest/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.bingin.databindingtest.databinding.LayoutMainBinding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,6 +17,23 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_main);
         LayoutMainBinding binding = DataBindingUtil.setContentView(this, R.layout.layout_main);
         binding.setStu(new Student("test1", "测试位置1"));
+        binding.setStr("str");
+        binding.setNum(8);
+        binding.setError(true);
+        binding.setArray(new String[]{"array0", "array1", "array2", "array3"});
+        binding.setArraykey(2);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("list0");
+        list.add("list1");
+        list.add("list2");
+        binding.setList(list);
+        binding.setListkey(1);
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("sex", "mapsex");
+        map.put("name", "mapname");
+        map.put("age", "mapage");
+        binding.setMap(map);
 
     }
 }
