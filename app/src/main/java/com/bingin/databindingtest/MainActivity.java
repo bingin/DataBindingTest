96f7bb4f470d6a67a7f1a7d4756e7e8efabc8f0f
Binglin Luo <binglinluo@gmail.com>
2018-12-20 13:52:47 +0800

    add a simple example

--- app/src/main/java/com/bingin/databindingtest/MainActivity.java
+++ app/src/main/java/com/bingin/databindingtest/MainActivity.java
@@ -1,13 +1,19 @@
 package com.bingin.databindingtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bingin.databindingtest.databinding.LayoutMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        LayoutMainBinding binding = DataBindingUtil.setContentView(this, R.layout.layout_main);
+        binding.setStu(new Student("test1", "测试位置1"));
+
     }
 }
